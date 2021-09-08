f705155af6953fa4c2bfc89009f37866be064076
yamanou <go02sleep@gmail.com>
2021-09-08 17:21:06 +0900

    Create custom icon

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -23,7 +23,7 @@ export default function MemoDetailScreen() {
         </Text>
       </ScrollView>
 
-      <CircleButton style={{ top: 160, button: 'auto' }} name="edit-2" />
+      <CircleButton style={{ top: 160, button: 'auto' }} name="pencil" />
     </View>
   );
 }
